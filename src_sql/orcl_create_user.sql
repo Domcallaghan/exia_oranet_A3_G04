c65baaa0b9c1e9811f9db877658d8379c19ccfe0
blag <blag@rakshata.com>
2015-02-10 17:51:00 +0100

    suite profil user

--- src_sql/orcl_create_user.sql
+++ src_sql/orcl_create_user.sql
@@ -11,8 +11,10 @@
 ALTER SYSTEM SET resource_limit=TRUE;
 
     ---------------------------------------------------
+    --                  PHARMAWEB                    --
+    ---------------------------------------------------
 
-    -- mise en place du profil avec les param de connexion
+    -- profil avec les param de connexion
 CREATE PROFILE prf_Pharmaweb
      LIMIT
          SESSIONS_PER_USER 1;
@@ -29,7 +31,11 @@ GRANT CONNECT, RESOURCE TO Pharmaweb;
 
     ---------------------------------------------------
 
-    -- mise en place du profil pour la team phara
+    ---------------------------------------------------
+    --                 PHARMATEAM                    --
+    ---------------------------------------------------
+
+    -- profil pour la team phara (prepa et pharma)
 CREATE PROFILE prf_PharmaTeam
      LIMIT
          SESSIONS_PER_USER 1
@@ -37,5 +43,21 @@ CREATE PROFILE prf_PharmaTeam
          CONNECT_TIME 60
          FAILED_LOGIN_ATTEMPTS 4
          PASSWORD_LIFE_TIME 45
+         ;
+         -- @todo : on fait quoi pour les exlude?
+
+    ---------------------------------------------------
+
+    ---------------------------------------------------
+    --                 UTILISATEURS                  --
+    ---------------------------------------------------
+
+    -- profil pour les utilisateurs
+CREATE PROFILE prf_User
+     LIMIT
+         SESSIONS_PER_USER 1
+         IDLE_TIME 60
+         FAILED_LOGIN_ATTEMPTS 3
+         PASSWORD_LIFE_TIME UNLIMITED
          ;
          -- @todo : on fait quoi pour les exlude?
