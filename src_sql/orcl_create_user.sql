37c2d80d090b0cb8626f21e762cbfbfeb4690318
blag <blag@rakshata.com>
2015-02-10 11:41:00 +0100

    debut du fichier de creation des users

---
+++ src_sql/orcl_create_user.sql
@@ -0,0 +1,14 @@
+    ---------------------------------------------------
+    ---------------------------------------------------
+    --                                               --
+    -- fichier de creation des utilisateurs et roles --
+    --                  By Benoit                    --
+    --                                               --
+    ---------------------------------------------------
+    ---------------------------------------------------
+
+    -- L'utilisateur principale Pharmaweb
+
+CREATE USER Pharmaweb IDENTIFIED BY Admin1337 DEFAULT TABLESPACE PHARMAWEB TEMPORARY TABLESPACE TEMP;
+    -- ajout de role de connexion et de modification
+GRANT CONNECT, RESOURCE TO Pharmaweb;
