01d6915268098648c57e428fca2d917a36f3f1d7
blag <blag@rakshata.com>
2015-02-11 16:16:22 +0100

    maj des tables /champs dans le dataSet

--- src_sql/orcl_jeux_de_test.sql
+++ src_sql/orcl_jeux_de_test.sql
@@ -14,14 +14,14 @@ INSERT INTO Centre_de_gestion VALUES(1,2, '326 avenue Foch', '', '', '76000', 'R
 INSERT INTO Centre_de_gestion VALUES(1,1, '2 Place Clichy', 'Batiment 3', '', '59000', 'Lille');
 INSERT INTO Centre_de_gestion VALUES(1,3, 'Galerie marchande', '', '', '75000', 'Paris');
 
-INSERT INTO Patient VALUES (1,1,1 ,'Durand','Annabelle','28-03-1994','3 rue de l église','','','75000', 'Paris', '245178963548156', '0645847915', '159753123654789', 'tierspayant', 25);
-INSERT INTO Patient VALUES (1,1,2 ,'Dupond','Martin','14-12-1985','12 rue des ciboulettes','Appartement 1','','85000', 'Juissieux', '147852369874563', '0232547896', '151515151515151', 'tierspayant', 75);
-INSERT INTO Patient VALUES (1,1,1 ,'Schejkowski','Anton','01-01-1999','20 boulevard des Parisiens','Appartement 6','','78000', 'Poissy', '123547896587452', '0232658975', '151515151515151', 'tierspayant',85);
+INSERT INTO Patient VALUES (1,1,1 ,'Durand','Annabelle','28-03-1994','3 rue de l église','','','75000', 'Paris', '245178963548156', '0645847915', '159753123654789', 'tierspayant', 25,'');
+INSERT INTO Patient VALUES (1,1,2 ,'Dupond','Martin','14-12-1985','12 rue des ciboulettes','Appartement 1','','85000', 'Juissieux', '147852369874563', '0232547896', '151515151515151', 'tierspayant', 75,'');
+INSERT INTO Patient VALUES (1,1,1 ,'Schejkowski','Anton','01-01-1999','20 boulevard des Parisiens','Appartement 6','','78000', 'Poissy', '123547896587452', '0232658975', '151515151515151', 'tierspayant',85,'');
 
-Insert into allergie values(1,'Foin');
-Insert into allergie values(1,'Poil de chat');
-Insert into allergie values(1,'Arachide');
-Insert into allergie values(1,'Acarien');
+Insert into ALLERGENE values(1,'Foin');
+Insert into ALLERGENE values(1,'Poil de chat');
+Insert into ALLERGENE values(1,'Arachide');
+Insert into ALLERGENE values(1,'Acarien');
 
 Insert into Etat values(1,'En attente');
 Insert into Etat values(2,'Accepté');
@@ -38,18 +38,14 @@ Insert into Ordonnance values (1,2,2,'https://ohfsdkwhf45','01/04/2014');
 Insert into Medicament values ('Paracéthamol','563541258965',1,'02/01/2014','azqdssqdzqesdzsdffeser',1,15,'biofarma');
 Insert into Medicament values ('Doliprane','155311541521',3,'02/01/2014','otipmodbshjbd',0,30,'servier');
 ----------------------------------------------------------------------
-Insert into Commande values (1,1,1,1256,1000,256,0);
-Insert into Commande values (1,3,2,2566,1400,166,1000);
+Insert into Commande values (1,1,1,125,100,256,0,0);
+Insert into Commande values (1,3,2,256,140,166,100,0);
 
 Insert into ligne_commande values ('Paracéthamol','563541258965',1,25,20);
 Insert into ligne_commande values ('Doliprane','155311541521',2,65,5);
 
-Insert into Fournir values(1,'Paracéthamol','563541258965',1000);
-Insert into Fournir values(2,'Paracéthamol','563541258965',200);
+Insert into Fournir values(1,'Paracéthamol','563541258965',1000,'01/01/2014',5124,0);
+Insert into Fournir values(2,'Paracéthamol','563541258965',200,'01/01/2014',514,0);
 
 Insert into INCOMPATIBILITE values(2,1);
 Insert into INCOMPATIBILITE values(1,3);
-
-Insert into RATTACHER values(1,4);
-Insert into RATTACHER values(2,2);
-
